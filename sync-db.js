6312bca667aa9354bd0fd78cd2266a59128c8fe2
sahanaadiga05 <sahanaadigav@gmail.com>
2026-03-27 22:37:02 +0530

    Updated invoice modules and added DB sync script

---
+++ sync-db.js
@@ -0,0 +1,24 @@
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, 'server/.env') });
+const sequelize = require('./server/config/database');
+const User = require('./server/auth/user.model');
+const Business = require('./server/businesses/business.model');
+const Client = require('./server/clients/client.model');
+const Invoice = require('./server/invoices/invoice.model');
+
+Business.hasMany(User, { foreignKey: 'businessId', onDelete: 'CASCADE' });
+User.belongsTo(Business, { foreignKey: 'businessId' });
+
+Business.hasMany(Client, { foreignKey: 'businessId', onDelete: 'CASCADE' });
+Client.belongsTo(Business, { foreignKey: 'businessId' });
+
+Business.hasMany(Invoice, { foreignKey: 'businessId', onDelete: 'CASCADE' });
+Invoice.belongsTo(Business, { foreignKey: 'businessId' });
+
+Client.hasMany(Invoice, { foreignKey: 'clientId', onDelete: 'CASCADE' });
+Invoice.belongsTo(Client, { foreignKey: 'clientId' });
+
+sequelize.sync({ alter: true }).then(() => {
+  console.log('Database synced successfully with alter: true');
+  process.exit(0);
+}).catch(console.error);
